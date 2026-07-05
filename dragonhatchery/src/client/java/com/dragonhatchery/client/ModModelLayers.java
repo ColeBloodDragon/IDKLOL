7965b186d8ea8575e9f9ef8d5ff6e6c24f2b1933
ColeBloodDragon <colefish2004@gmail.com>
2026-07-05 16:48:54 -0700

    Add files via upload

---
+++ dragonhatchery/src/client/java/com/dragonhatchery/client/ModModelLayers.java
@@ -0,0 +1,10 @@
+package com.dragonhatchery.client;
+
+import com.dragonhatchery.DragonHatchery;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.resources.Identifier;
+
+public class ModModelLayers {
+	public static final ModelLayerLocation BABY_ENDER_DRAGON = new ModelLayerLocation(
+			Identifier.fromNamespaceAndPath(DragonHatchery.MOD_ID, "baby_ender_dragon"), "main");
+}
