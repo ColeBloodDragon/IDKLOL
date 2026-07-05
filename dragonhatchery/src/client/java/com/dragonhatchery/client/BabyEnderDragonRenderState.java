d1099ae52192228caaa024ebc507b7abd71277e5
ColeBloodDragon <colefish2004@gmail.com>
2026-07-05 16:57:05 -0700

    Delete dragonhatchery directory

--- dragonhatchery/src/client/java/com/dragonhatchery/client/BabyEnderDragonRenderState.java
+++
@@ -1,8 +0,0 @@
-package com.dragonhatchery.client;
-
-import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
-
-/** Holds the per-frame data the model needs to animate a Baby Ender Dragon. */
-public class BabyEnderDragonRenderState extends LivingEntityRenderState {
-	public float flapAngle;
-}
